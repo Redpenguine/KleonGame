fdee31cd38fcef3dfb374dcd8e53c3b52f9dce58
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-24 16:00:46 +0300

    v1.10241600
    -Create the project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector2 direction;
+    
+    private Rigidbody2D playerRigidbody;
+    private Transform player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().GetComponent<Transform>();
+        playerRigidbody = FindObjectOfType<PlayerMovement>().GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0);
+        
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody.MovePosition(playerRigidbody.position + direction * Time.deltaTime);
+    }
+    
+}

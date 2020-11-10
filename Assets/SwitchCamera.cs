fb77020091bcba78b95d11ad3f752f804556ce11
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-11-11 01:33:25 +0200

    v1.1111200133
    - Add URP
    - Add SwitchCamera

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] cameras;
+
+    private int childrenCount;
+
+    private Transform player;
+    
+    private float minDistanse;
+    private GameObject curentCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        childrenCount = this.gameObject.transform.childCount;
+        cameras = new Transform[childrenCount];
+        for(int i = 0; i < childrenCount; i++)
+        {
+            cameras[i] = this.gameObject.transform.GetChild(i).gameObject.transform;
+            this.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        player = FindObjectOfType<PlayerMovement>().gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minDistanse = 100;
+        foreach(Transform camera in cameras)
+        {
+            if((camera.position - player.position).magnitude < minDistanse)
+            {
+                minDistanse = (camera.position - player.position).magnitude;
+                if(curentCamera != null)
+                {
+                    curentCamera.SetActive(false);
+                }
+                curentCamera = camera.gameObject;
+                curentCamera.SetActive(true);
+            }
+
+        }
+
+    }
+}

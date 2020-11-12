66ecc27721534662783224b4869cacdf09b4a31c
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-11-12 22:56:06 +0200

    v1.121120202256
    - Add game event system

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onInteractableObjectTriggerEnter;
+    public void InteractableObjectTriggerEnter()
+    {
+        onInteractableObjectTriggerEnter?.Invoke();
+    }
+
+    public event Action onInteractableObjectTriggerExit;
+    public void InteractableObjectTriggerExit()
+    {
+        onInteractableObjectTriggerExit?.Invoke();
+    }
+
+
+
+    
+}
